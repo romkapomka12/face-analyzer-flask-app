8a0121f9a2ec2c70834f97ac99667360c22af78b
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2025-09-13 19:28:13 +0300

    Prepare app for AI integration and load models on startup

--- app.py
+++ app.py
@@ -1,53 +1,51 @@
 from flask import Flask, render_template, request, url_for
 import base64
+from tensorflow import keras
+import numpy as np
+from PIL import Image
 import io
+import logging
 
+
+logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 
+models = {}
+CLASS_NAMES_GENDER = ['Female', 'Male'] # 0 = Female, 1 = Male
 
+def load_all_models():
+    """Завантажує AI моделі в пам'ять."""
+    logging.info("Завантаження моделей...")
+    try:
+        
+        models['gender'] = keras.models.load_model('models/gender_classifier_final.h5')
+        logging.info(">>> Модель для гендеру завантажено!")
+    except Exception as e:
+        logging.error(f"!!! Помилка завантаження моделей: {e}")
+
+
+@app.before_first_request
+def before_first_request():
+    load_all_models()
+
+
+"""ГОЛОВНІ (ROUTES)"""
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Сторінка, яка обробляє завантажений файл і показує результат
+
 @app.route('/analyze', methods=['POST'])
 def analyze():
-    # Перевіряємо, чи був файл завантажений
-    if 'file' not in request.files:
-        return "Помилка: файл не знайдено", 400
-    
-    file = request.files['file']
-
-    # Перевіряємо, чи користувач вибрав файл
-    if file.filename == '':
+  if 'file' not in request.files or request.files['file'].filename == '':
         return "Помилка: файл не вибрано", 400
 
-    if file:
-        # Читаємо байти зображення, щоб відобразити його на сторінці результатів
-        image_bytes = file.read()
-        # Кодуємо зображення в base64, щоб передати його в HTML
-        image_base64 = base64.b64encode(image_bytes).decode('utf-8')
-        
-        # --- ІМІТАЦІЯ РОБОТИ AI ---
-        # Тут, у майбутньому, буде реальний код для аналізу зображення
-        # вашими трьома моделями. А поки що повертаємо "заглушку".
-        results = {
-            "race": {
-                "prediction": "White",
-                "confidence": "88.14%"
-            },
-            "gender": {
-                "prediction": "Female",
-                "confidence": "95.20%"
-            },
-            "age": {
-                "prediction": 28
-            }
-        }
-        # ---------------------------
-
-
-        return render_template('result.html', results=results, image_file=image_base64)
+  file = request.files['file']
+  results = {
+        "race": {"prediction": "White", "confidence": "88.14%"},
+        "gender": {"prediction": "Female", "confidence": "95.20%"},
+        "age": {"prediction": 28}
+    }
 
 
 
