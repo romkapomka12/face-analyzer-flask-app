968842381e29d0f0157c625e3bc467f63c270bea
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2025-09-13 19:29:55 +0300

    integrate gender prediction model

--- app.py
+++ app.py
@@ -29,24 +29,33 @@ def before_first_request():
     load_all_models()
 
 
-"""ГОЛОВНІ (ROUTES)"""
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
-  if 'file' not in request.files or request.files['file'].filename == '':
+    if 'file' not in request.files or request.files['file'].filename == '':
         return "Помилка: файл не вибрано", 400
 
-  file = request.files['file']
-  results = {
-        "race": {"prediction": "White", "confidence": "88.14%"},
-        "gender": {"prediction": "Female", "confidence": "95.20%"},
-        "age": {"prediction": 28}
+    file = request.files['file']
+    image_bytes = file.read()
+    processed_image = preprocess_image(image_bytes)
+    
+
+    logging.info("Починаємо аналіз гендеру...")
+    pred_gender = models['gender'].predict(processed_image)[0][0]
+    predicted_gender = CLASS_NAMES_GENDER[1] if pred_gender > 0.5 else CLASS_NAMES_GENDER[0]
+    confidence_gender = f"{pred_gender:.2%}" if predicted_gender == 'Male' else f"{(1-pred_gender):.2%}"
+    logging.info(f"Гендер: {predicted_gender} ({confidence_gender})")
+    
+
+    results = {
+        "race": {"prediction": "White (mock)", "confidence": "N/A"},
+        "gender": {"prediction": predicted_gender, "confidence": confidence_gender},
+        "age": {"prediction": "28 (mock)"}
     }
 
+    
+    image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+    return render_template('result.html', results=results, image_file=image_base64)
 
 
 
